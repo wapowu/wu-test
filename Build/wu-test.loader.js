d12fdbbd5e367abe7f10f25f2ff1558ae7e8fb40
wapowu <wapowu3000@gmail.com>
2026-09-24 06:33:42 -0400

    newagainnofilters

--- Build/wu-test.loader.js
+++ Build/wu-test.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 59257358,
+    wasmFileSize: 59086139,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
